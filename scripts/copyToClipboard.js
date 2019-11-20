3843b6127a5ca3c7eef1f518bd495ce75686b7f6
Karol Lech <karollech787@gmail.com>
2019-11-20 10:33:24 +0100

    Update copyToClipboard.js

--- scripts/copyToClipboard.js
+++ scripts/copyToClipboard.js
@@ -1,16 +1,55 @@
-function copyToClipBoard(str) {
-    var element = document.createElement('textarea');
-    element.value = str;
-    document.body.appendChild(element);
-    element.select();
-    document.execCommand('copy');
-    document.body.removeChild(element);
-};
+window.Clipboard = (function (window, document, navigator) {
+    var textArea,
+        copy;
+
+    function isOS() {
+        return navigator.userAgent.match('/ipad/iphone/i');
+    }
+
+    function createTextArea(text) {
+        textArea = document.createElement('textarea');
+        textArea.value = text;
+        document.body.appendChild(textArea);
+    }
+
+    function selectText() {
+        var range,
+            selection;
+
+        if (isOS()) {
+            range = document.createRange();
+            range.selectNodeContents(textArea);
+            selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+            textArea.setSelectionRange(0, 999999);
+        } else {
+            textArea.select();
+        }
+    }
+
+    function copyToClipBoard() {
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    }
+
+    copy = function (text) {
+        createTextArea(text);
+        selectText();
+        copyToClipBoard();
+    };
+
+    return {
+        copy: copy
+    };
+
+})(window, document, navigator)
+
 
 document.getElementById("copyEmailMlodziKonkurs").addEventListener("click", function () {
-    copyToClipBoard('mlodzikonkurs@urz.pl');
+    Clipboard.copy('mlodzikonkurs@urz.pl');
 });
 
 document.getElementById("copyEmailMlodziKonferencja").addEventListener("click", function () {
-    copyToClipBoard('mlodzikonferencja@urz.pl');
+    Clipboard.copy('mlodzikonferencja@urz.pl');
 });
